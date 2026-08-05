6c597d795be3041d03e586508b37b7e7076c05fa
shevaramadhan <sheva08.ramadhan@gmail.com>
2026-08-05 15:14:10 +0700

    chore: clear backend code from main branch

--- scripts/init_all_tables.py
+++
@@ -1,6 +0,0 @@
-from app.database import engine
-from app.models import domain
-
-print("Memastikan seluruh tabel (termasuk ObstructionReport) ada di PostGIS...")
-domain.Base.metadata.create_all(bind=engine)
-print("Berhasil!")
