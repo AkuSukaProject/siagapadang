6f78eb49d38e3cca54f5e3d14229110d1980b2af
shevaramadhan <sheva08.ramadhan@gmail.com>
2026-08-05 15:09:50 +0700

    chore: remove experimental scripts from version control

--- scripts/test_photon.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-import json
-url = "https://photon.komoot.io/api/?q=Hotel+Ibis+Padang&limit=1"
-try:
-    response = urllib.request.urlopen(url)
-    print(json.loads(response.read().decode('utf-8')))
-except Exception as e:
-    print(e)
